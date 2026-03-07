4e9f3b8b57b7488c5f422ec453c8c1cc6c1f2252
David <davidgonzalez5910@gmail.com>
2026-03-07 17:12:47 -0500

    Se definen los métodos push, pop, peek, isEmpty

--- src/Pila.java
+++ src/Pila.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+
 public class Pila {
 
+    private ArrayList<Integer> miPila;
 
+    public Pila() {
+        this.miPila = new ArrayList<Integer>();
+    }
 
+    public void push(int value) {
+        miPila.add(value);
+    }
 
+    public int pop() {
+        if(isEmpty()) {
+            System.out.println("La pila está vacia...");
+            return -1;
+        } else {
+            int ultimoValor = miPila.getLast();
+            miPila.removeLast();
+
+            return ultimoValor;
+        }
+    }
+
+    public int peek() {
+        if(isEmpty()) {
+            System.out.println("No hay valores en la pila...");
+            return -1;
+        } else {
+            return miPila.getLast();
+        }
+    }
+
+    public boolean isEmpty() {
+        return miPila.isEmpty();
+    }
 }
